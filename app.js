8d765ecf87fb5120896d617f7924674c380d94e5
Arslan <muhammadarslanrajput12@gmail.com>
2024-03-25 17:39:06 -0700

    javascript example

---
+++ app.js
@@ -0,0 +1,26 @@
+// let user=+prompt("Write any number")
+// if(user%3){
+//     document.write("There is no division by 3")
+// }
+// else(document.write("division by 3"))
+
+
+
+// let userName=prompt("What is your name")
+// let na="Arslan"
+// if(userName.toLowerCase()===na.toLowerCase()){
+//     console.log("welcom to web page")
+// }
+
+
+
+let password=246810
+let passwordUser=+prompt("Type your password in number")
+if(password===passwordUser){
+    document.write("Right password")
+ 
+}
+else if(passwordUser===0){
+    document.write("please type password ")
+}
+else{document.write("try again")}
